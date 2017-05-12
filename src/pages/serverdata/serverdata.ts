84ad17f5283dfae275f4a8cd465e94f546953067
Mithun Nath Mayooranadhan <mithun-nath@outlook.com>
2017-05-12 15:18:42 -0400

    skelton template

--- src/pages/serverdata/serverdata.ts
+++ src/pages/serverdata/serverdata.ts
@@ -5,8 +5,10 @@ import 'rxjs/Rx';
 @Injectable()
 
 export class Serverdata {
+//7768038621167494812
+//6176425095225385540
 
-  private blogUrl: any = 'https://www.googleapis.com/blogger/v3/blogs/6176425095225385540/posts?fetchImages=false&status=live&maxResults=500&key=AIzaSyBp0a7ZCTaHTcAmo-bcnIYK1mpWL3yYWMY';
+  private blogUrl: any = 'https://www.googleapis.com/blogger/v3/blogs/7768038621167494812/posts?fetchImages=false&status=live&maxResults=500&key=AIzaSyBp0a7ZCTaHTcAmo-bcnIYK1mpWL3yYWMY';
   private searchUrl: any ='https://www.googleapis.com/blogger/v3/blogs/6176425095225385540/posts/search?q=oppam&fetchBodies=true&orderBy=published&fields=items&key=AIzaSyBp0a7ZCTaHTcAmo-bcnIYK1mpWL3yYWMY';
   posts: any;
   constructor(private http: Http) { }
